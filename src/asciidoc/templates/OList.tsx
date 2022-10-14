9850e6117913931bdb22d3b5da9aa14d0f75d9a5
benjaminleonard <benji@oxide.computer>
2022-10-14 17:27:49 +0100

    Fix OList styles

--- src/asciidoc/templates/OList.tsx
+++ src/asciidoc/templates/OList.tsx
@@ -7,7 +7,11 @@ const OList = ({ node }: { node: Asciidoctor.List }) => {
   return (
     <div className={`olist ${node.getStyle()} ${node.getRole()}`}>
       {node.hasTitle() && <div className="title">{node.getCaptionedTitle()}</div>}
-      <ol>
+      <ol
+        className={node.getStyle()}
+        reversed={node.isOption('reversed')}
+        start={node.getAttribute('start')}
+      >
         {node.getItems().map((item: Asciidoctor.ListItem, index) => (
           <li key={index} className={`${item.getRole()}`}>
             <p>{parse(item.getText())}</p>
