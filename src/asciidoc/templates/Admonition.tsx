978781f19c989be2ef55014a2686549daaa88cde
benjaminleonard <benji@oxide.computer>
2022-10-17 12:18:01 +0100

    Admonition style parity

--- src/asciidoc/templates/Admonition.tsx
+++ src/asciidoc/templates/Admonition.tsx
@@ -1,9 +1,22 @@
 import type { Asciidoctor } from 'asciidoctor'
+import parse from 'html-react-parser'
 
 import { Content } from '../'
 
 const Admonition = ({ node }: { node: Asciidoctor.Block }) => {
   const attrs = node.getAttributes()
+  const document = node.getDocument()
+
+  const title = node.hasTitle() && (
+    <div className="title">{parse(node.getCaptionedTitle())}</div>
+  )
+
+  const renderIcon = () =>
+    document.getAttribute('icons', 'font') && !attrs.icon ? (
+      <i className={`fa icon-${attrs.name}`} title={attrs.textlabel} />
+    ) : (
+      <img src={node.getIconUri(attrs.name)} alt={attrs.textlabel} />
+    )
 
   // Undocumented asciidoc attribute
   // Use this to check if we should render the content as is, or use a <Content /> block
@@ -12,15 +25,27 @@ const Admonition = ({ node }: { node: Asciidoctor.Block }) => {
 
   return (
     <div className={`admonitionblock ${attrs.name}`}>
-      <div className="admonition-icon"></div>
-      <div className="admonition-content">
-        <div className="adomonition-content-label">{attrs.textlabel}</div>
-        {contentModel === 'simple' ? (
-          <p dangerouslySetInnerHTML={{ __html: node.getContent() }} />
-        ) : (
-          <Content blocks={node.getBlocks()} />
-        )}
-      </div>
+      <table>
+        <tbody>
+          <tr>
+            <td className="icon">
+              {document.getAttribute('icons') ? (
+                renderIcon()
+              ) : (
+                <div className="title">{node.getAttribute('textlabel')}</div>
+              )}
+            </td>
+            <td className="content">
+              {title}
+              {contentModel === 'simple' ? (
+                parse(node.getContent())
+              ) : (
+                <Content blocks={node.getBlocks()} />
+              )}
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   )
 }
