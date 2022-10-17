1bd1d0b6c3e714be7355f88ea8072c120500bc2f
benjaminleonard <benji@oxide.computer>
2022-10-17 12:24:51 +0100

    Paragraph style parity

--- src/asciidoc/templates/Paragraph.tsx
+++ src/asciidoc/templates/Paragraph.tsx
@@ -1,8 +1,5 @@
 import type { Asciidoctor } from 'asciidoctor'
 
-// todo: fix hydration error
-// convert() is better than getContent() since it applies the substitutions
-// but it gives us hydration issues
 const Paragraph = ({ node }: { node: Asciidoctor.Block }) => {
   return (
     <div
@@ -10,7 +7,7 @@ const Paragraph = ({ node }: { node: Asciidoctor.Block }) => {
       className={`paragraph ${node.getRole() || ''}`}
     >
       {node.hasTitle() && <div className="title">{node.getCaptionedTitle()}</div>}
-      <p dangerouslySetInnerHTML={{ __html: node.convert() }} />
+      <p dangerouslySetInnerHTML={{ __html: node.getContent() }} />
     </div>
   )
 }
