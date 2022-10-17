1946085ab6c260d6f27ce422dfb5d0445355a596
benjaminleonard <benji@oxide.computer>
2022-10-17 15:48:16 +0100

    UList style parity

--- src/asciidoc/templates/UList.tsx
+++ src/asciidoc/templates/UList.tsx
@@ -3,19 +3,43 @@ import type { Asciidoctor } from 'asciidoctor'
 import { Content } from '../'
 
 const UList = ({ node }: { node: Asciidoctor.List }) => {
+  const isChecklist = node.isOption('checklist')
+
   return (
     <div
       id={node.getId ? node.getId() : ''}
-      className={`ulist ${node.getStyle() || ''} ${node.getRole() || ''}`}
+      className={`ulist ${node.getStyle() || ''} ${node.getRole() || ''} ${
+        isChecklist ? 'checklist' : ''
+      }`}
     >
       {node.hasTitle() && <div className="title">{node.getCaptionedTitle()}</div>}
-      <ul>
-        {node.getItems().map((item: Asciidoctor.ListItem, index) => (
-          <li key={index} id={item.getId()} className={item.getRole() || ''}>
-            <p dangerouslySetInnerHTML={{ __html: item.getText() }} />
-            <Content blocks={item.getBlocks()} />
-          </li>
-        ))}
+
+      <ul className={isChecklist ? 'checklist' : ''}>
+        {node.getItems().map((item: Asciidoctor.ListItem, index) => {
+          if (isChecklist) {
+            return (
+              <li key={index} id={item.getId()} className={item.getRole() || ''}>
+                <p>
+                  {isChecklist && item.hasAttribute('checkbox') && (
+                    <i
+                      className={`fa ${
+                        item.hasAttribute('checked') ? 'fa-check-square-o' : 'fa-square-o'
+                      }`}
+                    />
+                  )}{' '}
+                  {item.getText()}
+                </p>
+              </li>
+            )
+          } else {
+            return (
+              <li key={index} id={item.getId()} className={item.getRole() || ''}>
+                <p dangerouslySetInnerHTML={{ __html: item.getText() }} />
+                <Content blocks={item.getBlocks()} />
+              </li>
+            )
+          }
+        })}
       </ul>
     </div>
   )
