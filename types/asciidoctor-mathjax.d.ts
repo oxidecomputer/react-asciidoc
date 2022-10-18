8a79df6d6566f59b7fa0ed46778197101a34a20b
benjaminleonard <benji@oxide.computer>
2022-10-18 17:41:23 +0100

    Remove unused type file

--- types/asciidoctor-mathjax.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '@djencks/asciidoctor-mathjax' {
-  // eslint-disable-next-line @typescript-eslint/consistent-type-imports
-  type Registry = import('asciidoctor').Asciidoctor.Extensions.Registry
-  type Config = {}
-
-  function register(registry: Registry, config?: Config): Registry
-}
