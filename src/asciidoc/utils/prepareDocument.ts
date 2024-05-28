55cc06de5ecac9f58c4428697d8a3b4fc6a0bcf0
Benjamin Leonard <hello@benleonard.co.uk>
2024-05-28 13:05:19 +0100

    Fix type errors when remix compares jsonified manifest to type

--- src/asciidoc/utils/prepareDocument.ts
+++ src/asciidoc/utils/prepareDocument.ts
@@ -85,12 +85,12 @@ export type DocumentBlock = {
   attributes: Record<string, string | number>
   blocks: Block[]
   contentModel: ContentModel | undefined
-  footnotes: {
+  footnotes: ({
     text: string | undefined
     index: number | undefined
-  }[]
+  } | null)[]
   sections: DocumentSection[]
-  authors: { name: string | undefined; email: string | undefined }[]
+  authors: ({ name: string | undefined; email: string | undefined } | null)[]
 }
 
 export interface ParagraphBlock extends BaseBlock {
