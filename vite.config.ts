f209248a72a3f2b3336a8f6a2a32938223a3f5c9
Benjamin Leonard <hello@benleonard.co.uk>
2024-11-12 14:47:09 +0000

    Revert "Let's try ESM only"
    This reverts commit 53c0490bb8e8b6fc5d22724a769de2a66341ff9a.

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       entry: path.resolve(__dirname, 'src/asciidoc/index.tsx'),
       name: 'ReactAsciiDoc',
       fileName: 'react-asciidoc',
-      formats: ['es'],
+      formats: ['es', 'umd'],
     },
     rollupOptions: {
       external: ['react', '@asciidoctor/core', 'react-dom', 'html-react-parser'],
